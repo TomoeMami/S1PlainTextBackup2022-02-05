05d120b205871a9e9a79fcc84489f1fd01c9f23a
TomoeMami <trembleafterme@Outlook.com>
2022-02-26 21:04:04 +0800

    [2022-02-26 21:04:04]

--- autorun.py
+++ autorun.py
@@ -48,7 +48,7 @@ def parse_html(html,threadict):
     # titles = '['+titles+']'
     # return namelist,replylist,total_page,titles
 if __name__ == '__main__':
-    blacklist = [2050796,2050901,2047888,2048377,2049980,2053790]
+    blacklist = [2050796,2050901,2047888,2048377,2049980,2053790,2054713]
     sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8') #改变标准输出的默认编码
     # # 浏览器登录后得到的cookie，也就是刚才复制的字符串
     with open ('/home/ubuntu/s1cookie-1.txt','r',encoding='utf-8') as f:
